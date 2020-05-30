6cae11483630504cb11cfe4e741254e11f3f7d8a
github username <github email address>
2020-05-30 16:42:08 -0400

    Add user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,20 @@
 class SessionsController < ApplicationController
     def new
+        
     end
-    def create
+    def create   
+        user = User.find_by(email:params[:email])
+        if user && user.authenticate(params[:password])
+            session[:ser_id] = user.id
+            redirect_to recipes_path
+        else
+            raise "user could not be found"
+        end 
+
+
+    end
+    def destroy
+        reset_session
+        redirect_to recipes_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'signup', to: 'users#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
