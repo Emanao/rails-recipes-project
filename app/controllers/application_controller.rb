8c3e7f5d7ee40cd21f171a95b25f3bf6706b0fe0
github username <github email address>
2020-05-30 22:10:37 -0400

    Add helper methods: logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def authentication_required
+        if !logged_in?
+            redirect_to login_path
+        end
+    end
+    def logged_in?
+        !!current_user   
+    end
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+    end
 end
