aebb032efb542fb8cba0977ebb4c49f4e15d4745
github username <github email address>
2020-06-01 21:29:31 -0400

    Add new action and use bootstrap_form_for

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,8 +7,14 @@ class RecipesController < ApplicationController
         end
     end
     def new
+        @recipe = Recipe.new
+        @user = User.find(params[:user_id]) if params["user_id"].present? 
+    end
+    def create
+        
     end
     def show
         @recipe = Recipe.find(params[:id])
     end
+    
 end
