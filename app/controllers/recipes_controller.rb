32e6aff11b314683dd9e7d3e8a458aff2355ee12
github username <github email address>
2020-06-03 19:37:34 -0400

    Delete a user's recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,32 +1,51 @@
 class RecipesController < ApplicationController
     def index
         if params[:user_id].present?
-            user = User.find(params[:user_id])
-            @recipes = user.recipes
+            @user = User.find_by(id: params[:user_id]) 
+            @recipes = @user.recipes if !!@user
         else
             @recipes = Recipe.all.order("created_at DESC")
         end
     end
     def new
-        @recipe = Recipe.new
-        @user = User.find(params[:user_id]) if params[:user_id].present? 
+        if params[:user_id].present?
+            @user = User.find_by(id: params[:user_id])
+            @recipe = @user.recipes.build if !!@user
+        end
     end
     def create
-        user = User.find(params[:user_id]) if params[:user_id].present? 
-        new_recipe = user.recipes.build(recipe_params)
-        if new_recipe.save
-            redirect_to user_recipes_path(user)
-        else
-            raise params.inspect
+        user = User.find_by(id: params[:user_id]) if params[:user_id].present?
+        if user
+            new_recipe = user.recipes.build(recipe_params)
+            if new_recipe.save
+                redirect_to user_recipes_path(user)
+            else
+                raise params.inspect
+            end
         end
-
     end
     def show
         @recipe = Recipe.find(params[:id])
+        @user = @recipe.user
+    end
+    def edit
+        user = User.find_by(id: params[:user_id]) if params[:user_id].present?
+        if user
+            recipe = user.recipes.find(params[:id])
+        end
+    end
+    def destroy
+        user = User.find(params[:user_id]) if params[:user_id].present?
+        if user
+            user.recipes.find(params[:id]).destroy
+            redirect_to user_recipes_path user
+        end
     end
     private
     def recipe_params
         params.require(:recipe).permit(:title, :description, :directions, :servings)
     end
+        
+
 
 end
