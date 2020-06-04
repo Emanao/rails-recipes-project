d0458e4eba2c7e486bc1412a840b6693e83e5771
github username <github email address>
2020-06-04 10:27:53 -0400

    Add helper method render_recipe_collection

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,11 @@
 module RecipesHelper
+    def render_recipe_collection(partial_name, collection)
+        if collection.empty?
+            render :html => "<tr><td>No recipes to be displayed<t/r></td".html_safe
+        else
+            render :partial => "recipes/#{partial_name}", :collection => collection, as: :recipe
+        end
+    end
 end
+
+    
