dd684bfb18bec0b57308db766c6ee55175d4368d
github username <github email address>
2020-06-07 15:56:50 -0400

    Add new non-duplicated ingredients to the recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,20 @@
 class Recipe < ApplicationRecord
+    validates :title, presence:true
     belongs_to :user
 
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
 
     def ingredients_attributes=(ingredient_attributes)
-        ingredients_list = ingredient_attributes[:name].split("\n")
-        ingredients_list.collect do |ingredient_list|
-            ingredient = ingredient_list.split(",")[1].strip.downcase
-            self.ingredients.find_or_initialize_by(name: ingredient)
+
+        if !ingredient_attributes[:name].strip.empty?
+            ingredients_list = ingredient_attributes[:name].split(/\r\n/).reject{|ingredient_list| ingredient_list.empty?}
+
+            ingredients_list.map do |ingredient_list|
+                ingredient = Ingredient.find_or_create_by(name:ingredient_list)
+                self.recipe_ingredients.first_or_initialize(:ingredient=>ingredient)
+            end
         end
+
     end
 end
