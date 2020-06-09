be29aec78b85429a07cd8906b4fd999d92ebf561
github username <github email address>
2020-06-09 17:05:24 -0400

    Facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,23 +3,37 @@ class SessionsController < ApplicationController
         
     end
     def create   
-        user = User.find_by(email:params[:email])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to user_recipes_path user
-        else
-            if params[:email].strip.empty? || params[:password].empty?
-                flash[:credentials_alert] = "Email or password can not be blank."
+        
+        if !!auth_hash
+            user = User.find_or_create_by_omniauth(auth_hash)
+            if user
+                session[:user_id] = user.id 
+                redirect_to user_recipes_path user
             else
-                flash[:credentials_alert] = 'Email or password are incorrect.'
+                flash[:credentials_alert] = "User not found."
+                render :new
             end
-            render :new
-        end 
-
-
+        else
+            user = User.find_by(email:params[:email])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect_to user_recipes_path user
+            else
+                if params[:email].strip.empty? || params[:password].empty?
+                    flash[:credentials_alert] = "Email or password can not be blank."
+                else
+                    flash[:credentials_alert] = 'Email or password are incorrect.'
+                end
+                render :new
+            end 
+        end
     end
     def destroy
         reset_session
         redirect_to recipes_path
     end
+    private
+    def auth_hash
+        request.env['omniauth.auth']
+      end
 end
